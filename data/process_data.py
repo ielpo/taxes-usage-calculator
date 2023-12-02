01ec2e46c44dfa09bd73afc4f8bbd293285336ac
Tijanoscriptus <jot.tijan0405@gmail.com>
2023-12-02 11:57:51 +0100

    Created an initial setup for the script.

--- data/process_data.py
+++ data/process_data.py
@@ -1,1 +1,15 @@
-# Script to process input data into multiple JSON files
+"""Process Data 0.1
+This is a script for generating .json files out of the taxes statistics which
+are given by the government in form of excel-files.
+"""
+
+
+
+#Imports----------------------------------------------------------------------
+import pandas as pd
+
+
+
+#Run main program-------------------------------------------------------------
+if __name__ == "__main__":
+    print("I love Sakuya Izayoi!")
